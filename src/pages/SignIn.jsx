bcc05f561ee8de42d4c6c597b8914afe006ca475
kiseon77 <kieson.han77@gmail.com>
2024-08-25 12:31:45 +0900

    mission2

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import React from "react";
 import { useNavigate } from "react-router-dom";
+import NavBar from "../components/NavBar";
+import { useForm } from "react-hook-form";
 
 export default function SignIn() {
   const navigate = useNavigate();
-
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
   return (
     <>
-      <div>SignIn</div>
-      <div onClick={() => navigate("/signup")}>회원가입 하기</div>
+      <NavBar />
+      <form className="flex flex-col gap-7" onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="email"
+          placeholder="이메일"
+          {...register("email", {
+            required: "이메일을 입력해주세요.",
+          })}
+        />
+        <input
+          type="password"
+          placeholder="비밀번호"
+          {...register("password", {
+            required: true,
+          })}
+        />
+
+        <button>로그인</button>
+      </form>
+      <button onClick={() => navigate("/signup")}>회원가입 하기</button>
     </>
   );
 }
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,13 +1,61 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form";
 
 export default function SignUp() {
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  useEffect(() => {
+    console.log(errors.email.message);
+  }, [errors]);
+
   return (
     <div>
-      <form className="flex flex-col gap-7">
-        <input type="text" name="name" placeholder="이름" />
-        <input type="email" name="email" placeholder="이메일" />
-        <input type="password" name="password1" placeholder="비밀번호" />
-        <input type="password" name="password2" placeholder="비밀번호 확인" />
+      <form className="flex flex-col gap-7" onSubmit={handleSubmit(onSubmit)}>
+        <label for="inputName">이름 :</label>
+        <input
+          id="name"
+          type="text"
+          placeholder="이름"
+          {...register("name", {
+            required: "이름을 입력해주세요.",
+            pattern: {
+              value: /^[가-힣]$/,
+              message: "한글로 입력해주세요.",
+            },
+          })}
+          errors={errors}
+        />
+        <input
+          id="email"
+          type="email"
+          placeholder="이메일"
+          {...register("email", {
+            required: "이메일을 입력해주세요.",
+          })}
+        />
+        <input
+          type="password"
+          placeholder="비밀번호"
+          {...register("password", {
+            required: "비밀번호를 입력해주세요.",
+          })}
+        />
+        <input
+          type="passwordConfirm"
+          placeholder="비밀번호 확인"
+          {...register("passwordConfirm", {
+            required: "비밀번호를 입력해주세요.",
+            validate: (value) =>
+              watch().password1 !== value
+                ? "비밀번호가 일치하지 않습니다"
+                : true,
+          })}
+        />
         <button>회원가입</button>
       </form>
     </div>
